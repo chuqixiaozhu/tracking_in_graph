ac6e38c64a7923893586315487d914e4ac9ef81f
Piglet <chuqixiaozhu@gmail.com>
2016-06-20 22:19:01 +0800

    graph scenario

--- GRAPH_HEAD.h
+++ GRAPH_HEAD.h
@@ -12,8 +12,8 @@ class Vertex {
 public:
     Vertex(double x, double y) : x_(x), y_(y) { }
     Vertex() : x_(0), y_(0) { }
-    double get_x_();
-    double get_y_();
+    double get_x_() const;
+    double get_y_() const;
     void set_x_(double x);
     void set_y_(double y);
     void print() const;
@@ -22,11 +22,11 @@ private:
     double x_; //x coordinate
     double y_; //y coordinate
 };
-double Vertex::get_x_()
+double Vertex::get_x_() const
 {
     return this->x_;
 }
-double Vertex::get_y_()
+double Vertex::get_y_() const
 {
     return this->y_;
 }
@@ -79,19 +79,23 @@ double Node::fov = 10;
 //Graph Scenario
 const int MAX_VERTEX_NUM = 50;
 class Graph {
+    friend void init_vertexes(Graph &g, int row_num, int column_num);
+    friend void init_arcs(Graph &g, int row_num, int column_num);
+    friend void init_arcs_length(Graph &g);
+    friend void init_arcs_probs(Graph &g);
 public:
     Graph() { }
-    Vertex get_vertex(int index);
+    Vertex get_vertex(int index) const;
     void set_vertex(int index, int x, int y);
-    int get_arcs(int i, int j);
+    int get_arcs(int i, int j) const;
     void set_arcs(int x, int i, int j);
-    double get_arcs_length(int i, int j);
+    double get_arcs_length(int i, int j) const;
     void set_arcs_length(double l, int i, int j);
-    double get_arcs_probs(int i, int j);
+    double get_arcs_probs(int i, int j) const;
     void set_arcs_probs(double p, int i, int j);
-    int get_vertex_num();
+    int get_vertex_num() const;
     void set_vertex_num(int n);
-    int get_arc_num();
+    int get_arc_num() const;
     void set_arc_num(int i);
     void init_graph(int v_num = 20, int a_num = 31);
     
@@ -103,7 +107,7 @@ private:
     int vertex_num; //Number of vertexes
     int arc_num; //Number of arc
 };
-Vertex Graph::get_vertex(int index)
+Vertex Graph::get_vertex(int index) const
 {
     return this->vertexes[index];
 }
@@ -112,7 +116,7 @@ void Graph::set_vertex(int index, int x, int y)
     vertexes[index].set_x_(x);
     vertexes[index].set_y_(y);
 }
-int Graph::get_arcs(int i, int j)
+int Graph::get_arcs(int i, int j) const
 {
     return arcs[i][j];
 }
@@ -120,7 +124,7 @@ void Graph::set_arcs(int x, int i, int j)
 {
     arcs[i][j] = x;
 }
-double Graph::get_arcs_length(int i, int j)
+double Graph::get_arcs_length(int i, int j) const
 {
     return arcs_length[i][j];
 }
@@ -128,7 +132,7 @@ void Graph::set_arcs_length(double l, int i, int j)
 {
     arcs_length[i][j] = l;
 }
-double Graph::get_arcs_probs(int i, int j)
+double Graph::get_arcs_probs(int i, int j) const
 {
     return arcs_probs[i][j];
 }
@@ -136,7 +140,7 @@ void Graph::set_arcs_probs(double p, int i, int j)
 {
     arcs_probs[i][j] = p;
 }
-int Graph::get_vertex_num()
+int Graph::get_vertex_num() const
 {
     return vertex_num;
 }
@@ -144,7 +148,7 @@ void Graph::set_vertex_num(int n)
 {
     vertex_num = n;
 }
-int Graph::get_arc_num()
+int Graph::get_arc_num() const
 {
     return arc_num;
 }
@@ -152,26 +156,15 @@ void Graph::set_arc_num(int i)
 {
     arc_num = i;
 }
-void Graph::init_graph(int v_num, int a_num)
+void init_vertexes(Graph &g, int row_num, int column_num)
+/* Initial vertexes with their coordinates.
+ * 4 rows, 5 columns, an arc is between 16 to 24.
+ */
 {
-    vertex_num = v_num;
-    arc_num = a_num;
-
-    /* test */
-    //printf("vertex = %d, arc = %d\n", vertex_num, arc_num);
     random_device rd;
     mt19937 gen(rd());
-    uniform_real_distribution<> dist(8, 12);
-    for (int i = 0; i < 10; ++i) {
-        printf("random = %f\n", dist(gen));
-    }
-    /* /test */
-
-    /* Initial vertexes with their coordinates */
-    int row_num = 4; //Number of vertex rows
     int row_start = 18; //Coordinate start of vertex rows
     int row_end = 22; //Coordinate end of vertex rows
-    int column_num = 5; // Number of vertex columns
     int column_start = 8; //Coordinate start of vertex columns
     int column_end = 12; //Coordinate end of vertex columns
     double inc = 2 * Node::get_fov(); // Increase of coordinate 
@@ -185,7 +178,158 @@ void Graph::init_graph(int v_num, int a_num)
             double right = column_end + inc * j;
             uniform_real_distribution<> x_random(left, right);
             int index = i * column_num + j;
+            //Set coordinate
+            g.vertexes[index].set_x_(x_random(gen));
+            g.vertexes[index].set_y_(y_random(gen));
         }
     }
+    //test
+    for (int i = 0; i < g.vertex_num; ++i) {
+        printf("%d: (%f, %f)\n", i, g.vertexes[i].get_x_(), g.vertexes[i].get_y_());
+    }
 }
+void init_arcs(Graph &g, int row_num, int column_num)
+/* Initial the matrix arcs[][], set arc with 1, otherwise 0. */
+{
+    memset(g.arcs, 0, sizeof(g.arcs));
+    //printf("sizeof(g.arcs) = %d\n", sizeof(g.arcs));
+    for (int i = 1; i <= g.vertex_num - column_num; ++i) {
+        g.arcs[i - 1][i - 1 + column_num] = 1;
+        g.arcs[i - 1 + column_num][i - 1] = 1;
+        if (i % column_num != 0) {
+            g.arcs[i - 1][i] = 1;
+            g.arcs[i][i - 1] = 1;
+        }
+    }
+    for (int i = g.vertex_num - column_num + 1; i < g.vertex_num; ++i) {
+        g.arcs[i - 1][i] = 1;
+        g.arcs[i][i - 1] = 1;
+    }
+    //test
+    printf("  ");
+    for (int i = 0; i < g.vertex_num; ++i) {
+        printf("%3d", i);
+    }
+    fputc('\n', stdout);
+    for (int i = 0; i < g.vertex_num; ++i) {
+        printf("%-2d", i);
+        for (int j = 0; j < g.vertex_num; ++j) {
+            //printf("%3d", g.arcs[i][j]);
+            printf("%3d", g.get_arcs(i,j));
+        }
+        fputc('\n', stdout);
+    }
+}
+int is_arc(const Graph &g, int i, int j)
+/* If there is a edge between vertex i and j */
+{
+    return g.get_arcs(i, j);
+}
+double distance_vertexes(const Vertex &vi, const Vertex &vj)
+{
+    double xi = vi.get_x_();
+    double yi = vi.get_y_();
+    double xj = vj.get_x_();
+    double yj = vj.get_y_();
+
+    return sqrt(pow(xi - xj, 2) + pow(yi - yj, 2));
+}
+void init_arcs_length(Graph &g)
+/* Calculate lengths of arcs.
+ */
+{
+    memset(g.arcs_length, 0, sizeof(g.arcs_length));
+    for (int i = 0; i < g.vertex_num; ++i) {
+        for (int j = 0; j < g.vertex_num; ++j) {
+            if (!is_arc(g, i, j)) {
+                continue;
+            }
+            double dist = distance_vertexes(g.vertexes[i], g.vertexes[j]);
+            g.arcs_length[i][j] = dist;
+            g.arcs_length[j][i] = dist;
+        }
+    }
+    //test
+    printf("  ");
+    for (int i = 0; i < g.vertex_num; ++i) {
+        printf("%5d", i);
+    }
+    fputc('\n', stdout);
+    for (int i = 0; i < g.vertex_num; ++i) {
+        printf("%-2d", i);
+        for (int j = 0; j < g.vertex_num; ++j) {
+            //printf("%3d", g.arcs[i][j]);
+            printf("%5.1f", g.get_arcs_length(i,j));
+        }
+        fputc('\n', stdout);
+    }
+}
+void init_arcs_probs(Graph &g)
+/* Calculate probabilities of moving in arcs */
+{
+    memset(g.arcs_probs, 0, sizeof(g.arcs_probs));
+    random_device rd;
+    mt19937 gen(rd());
+    for (int v = 0; v < g.vertex_num; ++v) {
+        /* Out Degree of vertex v */
+        int degree = 0;
+        for (int j = 0; j < g.vertex_num; ++j) {
+            if (g.arcs[v][j]) {
+                ++degree;
+            }
+        }
+        double cumulative_prob = 0;
+        for (int j = 0; j < g.vertex_num; ++j) {
+            if (!g.arcs[v][j]) {
+                continue;
+            }
+            --degree;
+            if (degree == 0) {
+                g.arcs_probs[v][j] = 1 - cumulative_prob;
+            } else {
+                uniform_real_distribution<> prob_random(0, 1 - cumulative_prob);
+                double prob_vj = prob_random(gen);
+                g.arcs_probs[v][j] = prob_vj;
+                cumulative_prob += prob_vj;
+            }
+        }
+    }
+    //test
+    printf("  ");
+    for (int i = 0; i < g.vertex_num; ++i) {
+        printf("%7d", i);
+    }
+    fputc('\n', stdout);
+    for (int i = 0; i < g.vertex_num; ++i) {
+        printf("%-2d", i);
+        for (int j = 0; j < g.vertex_num; ++j) {
+            //printf("%3d", g.arcs[i][j]);
+            printf("%7.4f", g.get_arcs_probs(i,j));
+        }
+        fputc('\n', stdout);
+    }
+}
+void Graph::init_graph(int v_num, int a_num)
+{
+    vertex_num = v_num;
+    arc_num = a_num;
+    int row_num = 4; //Number of vertex row
+    int column_num = 5; //Number of vertex column
+
+    /* Initial vertexes with their coordinates */
+    init_vertexes(*this, row_num, column_num);
+
+    /* Initial the matrix of arcs */
+    init_arcs(*this, row_num, column_num);
+
+    /* Calculate the matrix of lengths of arcs */
+    init_arcs_length(*this);
+
+    /* Calculate the probabilities of moving in arcs */
+    init_arcs_probs(*this);
+
+
+}
+Node target;
+const int END = 100; //Experiment time
 #endif
--- main.cpp
+++ main.cpp
@@ -6,17 +6,26 @@
 
 void test()
 {
-    Node n(2, 3);
+    /*Node n(2, 3);
     n.print();
     n.set_x_(10);
     n.set_y_(10);
-    n.print();
+    n.print();*/
     Graph museum;
     museum.init_graph();
+    int a[2][3] = {
+        {1, 3, 4},
+        {2, 5, 9}
+    };
+    printf("a[1][2] = %d\n", a[1][2]);
+    memset(a, 0, sizeof(a));
+    printf("sizeof(a) = %d\n", sizeof(a));
+    printf("a[1][2] = %d\n", a[1][2]);
 }
 
 int main(int argc, char *argv[])
 {
+    freopen("output.txt", "w", stdout);
     test();
     return 0;
 }
